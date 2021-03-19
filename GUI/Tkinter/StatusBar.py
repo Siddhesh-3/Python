333aca8e5b78c1b2a4b20ca38ba8615ca160a3ee
Siddhesh-3 <siddhesh.gaykar017@gmail.com>
2021-03-19 18:51:59 +0530

    status bar

---
+++ GUI/Tkinter/StatusBar.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+
+
+root = Tk()
+root.geometry("455x233")
+root.title("Status bar tutorial")
+
+#function
+
+def upload():
+    statusvar.set("Busy..")
+    sbar.update()
+    import time
+    time.sleep(2)
+    statusvar.set("Ready")
+
+
+#
+
+statusvar = StringVar()
+statusvar.set("Ready")
+
+sbar = Label(root, textvariable=statusvar, relief=SUNKEN, anchor="w")
+sbar.pack(side=BOTTOM, fill=X)
+
+Button(root, text="Upload", command=upload).pack()
+
+root.mainloop()
