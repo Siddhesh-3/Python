7ede28b4447e390874eab626a0c6df5d342bf536
Siddhesh-3 <siddhesh.gaykar017@gmail.com>
2021-03-19 14:31:53 +0530

    listbox

---
+++ GUI/Tkinter/Listbox.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("455x350")
+root.title("ListBox ")
+
+#function
+
+def add():
+    global i
+    lbx.insert(ACTIVE, f"value : {i}")
+    i+=1 
+    
+i = 0
+#
+lbx = Listbox(root)
+lbx.pack()
+lbx.insert(END,"First Item of Listbox")
+
+Button(root , text = "Add" , command=add).pack()
+
+
+root.mainloop()
