b30816c548bcf4eddbc005a7de5f5289ddce5977
Siddhesh-3 <siddhesh.gaykar017@gmail.com>
2021-03-24 12:31:30 +0530

    Notepad 1.0

---
+++ GUI/Tkinter/Notepad.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+
+if __name__ == '__main__':
+    #basic setup
+    
+    root = Tk()
+    root.title("Untitled - Notepad")
+    root.geometry("644x788")
+    
+    root.mainloop()
